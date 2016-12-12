c8ddf5454016a2be2937286e94c26d11947a1393
hzq <hzq@hzqdeMacBook-Air.local>
2016-12-12 14:03:02 +0800

    修改版本管理配置

--- service-order/src/main/java/com/hzq/order/OrderServiceProvider.java
+++ service-order/src/main/java/com/hzq/order/OrderServiceProvider.java
@@ -3,7 +3,7 @@ package com.hzq.order;
 /**
  * Created by hzq on 16/12/10.
  */
-public class OrderProvider {
+public class OrderServiceProvider {
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
     }
