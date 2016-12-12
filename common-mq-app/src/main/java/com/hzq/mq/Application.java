e339bf47f6d2f98343d63a486fdb12e956006fd2
Your Name <you@example.com>
2016-12-13 00:18:47 +0800

    配置优化

--- common-mq-app/src/main/java/com/hzq/mq/Application.java
+++ common-mq-app/src/main/java/com/hzq/mq/Application.java
@@ -2,6 +2,7 @@ package com.hzq.mq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @ImportResource("application-spring.xml")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
