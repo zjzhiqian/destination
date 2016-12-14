43650ec2204ee584495506b278371150d59c878c
hzq <hzq@hzqdeMacBook-Air.local>
2016-12-14 17:48:58 +0800

    提交设置

--- service-accounting/src/main/java/com/hzq/accounting/AccountingServiceProvider.java
+++ service-accounting/src/main/java/com/hzq/accounting/AccountingServiceProvider.java
@@ -1,11 +1,5 @@
 package com.hzq.accounting;
 
-import com.hzq.accounting.entity.Accounting;
-import com.hzq.accounting.service.AccountingService;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created by hzq on 16/12/13.
@@ -13,29 +7,5 @@ import java.util.concurrent.Executors;
 public class AccountingServiceProvider {
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
-//
-//        try {
-//            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-spring.xml");
-//            context.start();
-//
-//
-//
-//            AccountingService service = context.getBean("accountingService", AccountingService.class);
-//
-//            ExecutorService pool = Executors.newFixedThreadPool(5);
-//            Accounting accounting = new Accounting();
-//            accounting.setVoucherNo("123213");
-//
-//            pool.execute(()-> service.generateAccounting(accounting));
-//            pool.execute(()-> service.generateAccounting(accounting));
-//            pool.execute(()-> service.generateAccounting(accounting));
-//            pool.execute(()-> service.generateAccounting(accounting));
-//            pool.execute(()-> service.generateAccounting(accounting));
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-
     }
 }
