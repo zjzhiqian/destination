ab7591db3d4a4653166e5fd2e03783fa63b8ed6b
hzq <hzq@hzqdeMacBook-Air.local>
2016-12-19 10:05:28 +0800

    dubbo引入文件重命名

--- springboot-app/src/main/java/com/hzq/springboot/App.java
+++ springboot-app/src/main/java/com/hzq/springboot/App.java
@@ -8,10 +8,9 @@ import org.springframework.context.annotation.ImportResource;
  * Created by hzq on 16/12/8.
  */
 @SpringBootApplication
-@ImportResource("classpath:dubbo.xml")
+@ImportResource("classpath:application-dubbo-consumer.xml")
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(1);
         SpringApplication.run(App.class, args);
     }
 }
